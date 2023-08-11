358f3cd60f8ddb802def78eb52d5df3b75955a55
Abijah Ebe <Abijahebe911@gmail.com>
2023-08-11 11:38:21 +0000

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,12 @@
 # A puppet manuscript to replace a line in a file on a server
 
-$file_to_edit = '/var/www/html/wp-settings.php'
+file { '/etc/apache2/sites.available/000-defaultconf':
+  ensure  => present,
+  content => template('apache2/000-default.conf.erb'),
+  notify  => Service['apache2'],
+}
 
-#replace line containing "phpp" with "php"
-
-exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path	  => ['/bin','/usr/bin']
+service { 'apache2':
+  ensure => running,
+  enable => true,
 }
